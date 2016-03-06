d7df35c7de174ae98c4f5e99dffc5b6a81ee3a8c
Jay Hunt <jaythecelt@gmail.com>
2016-03-06 15:44:36 -0500

    Update b.java

--- b.java
+++ b.java
@@ -5,6 +5,7 @@ public class B {
 public void method2 {
 
   System.out.println("This is an original line in method2");
+  System.out.println("Added this line in master. We DO NOT want this line in my-branch")
 }
 
 
