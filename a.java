ecfc39472479d3ffce90943ba366f2942dc9eed5
Jay Hunt <jaythecelt@gmail.com>
2016-03-06 15:43:36 -0500

    Update a.java

--- a.java
+++ a.java
@@ -4,7 +4,8 @@ public class A {
 
 public void method1 {
 
-  System.out.println("Original line");
+  System.out.println("Changed this original line on master.  We want this change on my-branch");
+  System.out.println("Added this line on master.  We do not want this change on my-branch");
 }
 
 }
